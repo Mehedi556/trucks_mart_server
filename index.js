98d2361df4fe4d3808671415c00cb9abaa713f12
mehedi556 <mehe556di@gmail.com>
2022-11-28 06:13:43 +0600

    adding deleting api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //Middleware
@@ -32,6 +32,13 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    app.delete('/members/:id' , async(req , res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
 
     app.post('/productDetails', async (req, res) => {
       const productDetails = req.body;
